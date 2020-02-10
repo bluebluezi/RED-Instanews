b51fc12305c9ef4fbfc0b681573984780d1d39e0
Johnny Ng <ng.johnnyzi@gmail.com>
2020-02-10 15:34:38 -0500

    Chore: initial project files and gulp linked

---
+++ js/app.js
@@ -0,0 +1,51 @@
+// let el = document.getElementsByClassName('sub-header')
+// console.log(el);
+// key to jquery is the $ sign object
+// jquery version: let el = $(".sub-header")
+// $(function() {
+// 	let counter = 5;
+
+// 	$('.page-wrapper').on('click', function() {
+// 		// counter++;
+// 		// $('.numbers').append(`<li>${counter}</li>`);
+
+// 		let val = $('input[type="text"]').val();
+// 		console.log(val);
+// 		$('.page-wrapper').append(val);
+// 		$("input[type='text']").val('');
+// 	});
+
+// 	$('form').after('<button>Check Value</button>');
+// });
+
+$(function () {
+
+
+	$('.select-dropdown').change(function (e) {
+		$(".article-grid").empty();
+
+		let selectedVal = $(".select-dropdown option:selected").text();
+		$.getJSON(`https://api.nytimes.com/svc/topstories/v2/${selectedVal}.json?api-key=f2IdtWRfwa37ubY3f2wGJ5MDReLY3klv`,
+			function ({ results }) {
+				console.log(results);
+				let image, caption, articleLink;
+				$.each(results, function (key, val) {
+					//loading gif
+					if (key > 11) {
+						return false;
+					}
+					image = val.multimedia[0].url;
+					console.log();
+					articleLink = val.url;
+					caption = val.abstract;
+					$(".article-grid").append(`<a href="${articleLink}"><figure class="cell${key}"><img src="${image}" /img><p>${caption}</p></figure></a>`)
+
+				});
+			});
+
+	});
+
+});
+
+
+
