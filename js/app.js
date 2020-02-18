779438ce6cda68580a3b1f684521b2e386f611fa
Johnny Ng <ng.johnnyzi@gmail.com>
2020-02-17 20:02:20 -0500

    style:Optimized article grid layout for mobile, tablet and desktop

--- js/app.js
+++ js/app.js
@@ -1,6 +1,57 @@
-const API_KEY = 'f2IdtWRfwa37ubY3f2wGJ5MDReLY3klv';
 
-$(function () {
+$(function () { //run after jQuery loaded
+
+	const API_KEY = 'f2IdtWRfwa37ubY3f2wGJ5MDReLY3klv';
+	let vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	let vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+	let cachedResults = null;
+
+	function calculateRowCount() {
+		if (vw < 768) {
+			return 1;
+		} else if (vw < 992) {
+			return 3
+		} else {
+			return 4;
+		}
+	}
+
+	function renderGrid() {
+		console.log('rerendering');
+		$(".article-grid").empty();
+		const rowCount = calculateRowCount();
+		$.each(cachedResults, function (key, val) {
+			let image, caption, articleLink;
+			image = val.multimedia[0].url;
+
+
+			if (image != null) {
+
+				let width = val.multimedia[0].width;
+				let height = val.multimedia[0].height;
+				if (1.5 * width > height) {
+					height = vw / rowCount * 1.5;
+					width = 'auto';
+				} else {
+					width = vw / rowCount;
+					height = 'auto';
+				}
+
+				articleLink = val.url;
+				caption = val.abstract || val.title;
+				$(".article-grid").append(`
+				<a href="${articleLink}" class="grid-links">
+					<figure class="cell-${key}">
+						<img src="${image}" width="${width}" height="${height}"/img>
+						<figurecaption>
+							<p>${caption}</p>
+						</figurecaption>
+					</figure>
+				</a>`)
+			}
+		});
+	}
+
 	//add snap to first article feature..
 	$(".dropdown select").change(function (e) {
 		$(".article-grid").empty();
@@ -9,53 +60,19 @@ $(function () {
 		$.getJSON(`https://api.nytimes.com/svc/topstories/v2/${selectedVal}.json?api-key=${API_KEY}`,
 			function ({ results }) {
 				console.log(results);
-				let image, caption, articleLink;
-				//let height = $(".nav-area").height();
-				//let offset = height + $(".nav-area").offset().top;
-				//$(".article-grid").offset({ top: offset }); //position of .article-grid set here
-				//$(".nav-area").after (place loading.gif)	, also add class to it				//loading gif
-				//be sure to style loading so the gif take up whole screen (min-height:100vh, width auto, object fit cover)
-				$.each(results, function (key, val) {
+				cachedResults = results
 
-					image = val.multimedia[0].url;
-
-					if (image != null) {
-
-						// if (height > val.multimedia[0].height) {
-						// 	height = val.multimedia[0].height;
-						// 	console.log("NEWEST MIN HEIGHT IS" + height);
-						// }
-
-						articleLink = val.url;
-						caption = val.abstract;
-						$(".article-grid").append(`
-						<a href="${articleLink}" class="grid-links">
-							<figure class="cell-${key}">
-								<img src="${image}" /img>
-								<figurecaption>
-									<p>${caption}</p>
-								</figurecaption>
-							</figure>
-						</a>`)
-					}
-				});
-				//hide nav.area after
-				//end of loading of images
-				// let scrollPosition = $(".article-grid").position();
-				// //add softscroll
-				// window.scrollTo(scrollPosition);
-				// console.log("scrolled");
-				// let articleHeight = $(".article-grid").height();
-				// let footerOffset = articleHeight + offset;
-				// console.log(typeof articleHeight);
-				// console.log(articleHeight);
-				// console.log(footerOffset);
-				// $("footer").offset({ top: footerOffset });
-
+				renderGrid();
 			});
-
 	});
 
+	$(window).resize(function () {
+		vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+		vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+		if (cachedResults !== null) {
+			renderGrid();
+		}
+	})
 });
 
 
